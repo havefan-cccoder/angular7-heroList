6d91103e68ba2523763e7701b29a02c3659aee3c
zhumingjie <zmj@enn.com>
2019-04-03 10:58:03 +0800

    添加login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { HeroesComponent } from './heroes/heroes.component'; /* 导入需要展示的路由组件 */
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { from } from 'rxjs';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent},
   { path: 'heroes', component: HeroesComponent}, /* 配置路由信息 */
   { path: 'dashboard', component: DashboardComponent}, /* 配置路由信息 */
   { path: 'detail/:id', component: HeroDetailComponent} /* 配置路由信息,path 中的冒号（:）表示 :id 是一个占位符，它表示某个特定英雄的 id */
